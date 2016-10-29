d9bfe4b2ff145230f3288276f5b732beb647b551
Zhou ZENG <zhouzeng@ZhoudeMacBook-Air.local>
2016-10-29 12:24:46 +0800

    admin can set the order_status

--- app/controllers/admin/orders_controller.rb
+++ app/controllers/admin/orders_controller.rb
@@ -6,4 +6,24 @@ class Admin::OrdersController < ApplicationController
   def index
     @orders = Order.all
   end
+
+  def edit
+    @order = Order.find(params[:id])
+  end
+
+  def update
+    @order = Order.find(params[:id])
+
+    if @order.update(order_params)
+      redirect_to admin_orders_path
+    else
+      render :edit
+    end
+  end
+
+  private
+
+  def order_params
+    params.require(:order).permit(:user_id, :product_id, :order_status)
+  end
 end
